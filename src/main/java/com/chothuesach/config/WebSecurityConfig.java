8155c37516aea549a60b65a4c8d60434b23120d1
ntduy96 <ntduy96@gmail.com>
2018-03-31 00:20:33 +0700

    add configuration to access static contents (html, css, js)

--- src/main/java/com/chothuesach/config/WebSecurityConfig.java
+++ src/main/java/com/chothuesach/config/WebSecurityConfig.java
@@ -1,6 +1,8 @@
 package com.chothuesach.config;
 
+import com.chothuesach.service.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -12,7 +14,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import com.chothuesach.service.MyUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
@@ -50,6 +51,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
+				.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                 .antMatchers("/", "/home", "/signup", "/authority", "/user", "/resource/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
