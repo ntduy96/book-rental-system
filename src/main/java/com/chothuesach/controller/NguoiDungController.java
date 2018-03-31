9bbd70d7f15ca7f99a4324be2519e4bc71169c0d
ntduy96 <ntduy96@gmail.com>
2018-04-01 01:57:59 +0700

    add rest method to check for valid form field

--- src/main/java/com/chothuesach/controller/NguoiDungController.java
+++ src/main/java/com/chothuesach/controller/NguoiDungController.java
@@ -1,24 +1,47 @@
 package com.chothuesach.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.chothuesach.model.NguoiDung;
-import com.chothuesach.repository.NguoiDungRepository;
+import com.chothuesach.service.NguoiDungService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 
 @RestController
-@RequestMapping("/nguoidung")
+@RequestMapping("/api/nguoidung")
 public class NguoiDungController {
 	
 	@Autowired
-	private NguoiDungRepository nguoiDungRepository;
+	private NguoiDungService nguoiDungService;
 
 	@GetMapping("/{username}")
 	public NguoiDung getByUsername(@PathVariable String username) {
-		return nguoiDungRepository.findOneByTenNguoiDung(username);
+		return nguoiDungService.getOneByUsername(username);
 	}
+
+	@PostMapping("/check")
+    public HashMap<String, Boolean> checkExist(@RequestParam(required = false) String tenNguoiDung, @RequestParam(required = false) String email, @RequestParam(required = false) String soCmnd) {
+
+	    if (tenNguoiDung != null) {
+	        return addJsonField(nguoiDungService.tenNguoiDungExist(tenNguoiDung));
+        }
+
+        if (email != null) {
+            return addJsonField(nguoiDungService.emailExist(email));
+        }
+
+        if (soCmnd != null) {
+            return addJsonField(nguoiDungService.soCmndExist(soCmnd));
+        }
+
+        return null;
+
+    }
+
+    private HashMap<String, Boolean> addJsonField(boolean result) {
+	    HashMap<String, Boolean> json = new HashMap<>();
+	    json.put("exist", result);
+	    return json;
+    }
 	
 }
