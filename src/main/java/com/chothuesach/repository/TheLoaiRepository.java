632b6858faf18145ee366dedb33f14513af56d78
ntduy96 <ntduy96@gmail.com>
2018-04-05 00:40:24 +0700

    add method findAll and getBySlug in TheLoaiRepository

--- src/main/java/com/chothuesach/repository/TheLoaiRepository.java
+++ src/main/java/com/chothuesach/repository/TheLoaiRepository.java
@@ -1,9 +1,14 @@
 package com.chothuesach.repository;
 
+import com.chothuesach.model.TheLoai;
 import org.springframework.data.repository.CrudRepository;
 
-import com.chothuesach.model.TheLoai;
+import java.util.List;
 
 public interface TheLoaiRepository extends CrudRepository<TheLoai, Long> {
+
+    List<TheLoai> findAll();
+
+    TheLoai getBySlug(String slug);
 
 }
