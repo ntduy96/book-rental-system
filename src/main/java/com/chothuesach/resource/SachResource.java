ffa30ed01185f91796d9d4fd10fcc09868ed6f08
ntduy96 <ntduy96@gmail.com>
2018-04-05 00:56:57 +0700

    get all sach sorted default by ngayTao desc

--- src/main/java/com/chothuesach/resource/SachResource.java
+++ src/main/java/com/chothuesach/resource/SachResource.java
@@ -1,28 +1,19 @@
 package com.chothuesach.resource;
 
-import java.util.HashMap;
-import java.util.List;
-
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.chothuesach.dto.SachDto;
 import com.chothuesach.jsonview.SachView;
 import com.chothuesach.model.Sach;
 import com.chothuesach.service.SachService;
 import com.fasterxml.jackson.annotation.JsonView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
 
 @RestController
 @RequestMapping("/resource/sach")
@@ -33,8 +24,8 @@ public class SachResource {
 	
 	@GetMapping
 	@JsonView(SachView.Overview.class)
-	public Page<Sach> getAll(@PageableDefault(page = 0, size = 5) Pageable pageable) {
-		return sachService.getAllSach(pageable);
+	public List<Sach> getAll(@PageableDefault(page = 0, size = 10, sort = {"ngayTao"}, direction = Sort.Direction.DESC) Pageable pageable) {
+		return sachService.searchByTenSach("", pageable);
 	}
 	
 	@GetMapping("/search")
@@ -79,5 +70,11 @@ public class SachResource {
 		response.put("deleted", "ok");
 		return response;
 	}
-	
+
+	@GetMapping("/theloai/{theLoai}")
+	@JsonView(SachView.Overview.class)
+	public List<Sach> getSachByTheLoai(@PathVariable String theLoai) {
+		return sachService.getSachByTheLoai(theLoai);
+	}
+
 }
