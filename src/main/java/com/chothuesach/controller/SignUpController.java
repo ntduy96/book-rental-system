b06d20011f7dbd90ee6c1f0f638a86f127e4fa9d
ntduy96 <ntduy96@gmail.com>
2018-04-04 22:38:12 +0700

    catch exception when email exists in db

--- src/main/java/com/chothuesach/controller/SignUpController.java
+++ src/main/java/com/chothuesach/controller/SignUpController.java
@@ -1,22 +1,22 @@
 package com.chothuesach.controller;
 
-import javax.validation.Valid;
-
+import com.chothuesach.dto.NguoiDungDto;
+import com.chothuesach.exception.EmailExistsException;
+import com.chothuesach.model.NguoiDung;
+import com.chothuesach.service.NguoiDungService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.chothuesach.dto.NguoiDungDto;
-import com.chothuesach.service.NguoiDungService;
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/signup")
@@ -36,13 +36,22 @@ public class SignUpController {
 	@RequestMapping(method = RequestMethod.POST)
 	@PreAuthorize("hasRole('ROLE_ANONYMOUS')")
 	public ModelAndView registerUserAccount(@ModelAttribute("user") @Valid NguoiDungDto nguoiDungDto,
-			BindingResult result) {
+											BindingResult result, ModelMap model) {
 		if (result.hasErrors()) {
-			return new ModelAndView("signup");
+			return new ModelAndView("redirect:signup");
 		}
-		if (nguoiDungService.registerNewNguoiDung(nguoiDungDto) != null) {
-			return new ModelAndView("login");
+		try {
+			NguoiDung newNguoiDung = nguoiDungService.registerNewNguoiDung(nguoiDungDto);
+			if (newNguoiDung != null) {
+				model.addAttribute("success", true);
+				return new ModelAndView("redirect:login", model);
+			}
+		} catch (EmailExistsException e) {
+			model.addAttribute("exist", true);
+			model.addAttribute("message", "Email " + nguoiDungDto.getEmail() + " already existed!");
+			return new ModelAndView("redirect:login", model);
 		}
+
 		return new ModelAndView("signup");
 	}
 
