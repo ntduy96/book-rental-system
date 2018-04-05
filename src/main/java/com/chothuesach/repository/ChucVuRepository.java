bffe2efc5b47794a9a47c2a22f37ee80259b72fa
ntduy96 <ntduy96@gmail.com>
2018-04-06 02:41:51 +0700

    add some methods in ChucVuRepository

--- src/main/java/com/chothuesach/repository/ChucVuRepository.java
+++ src/main/java/com/chothuesach/repository/ChucVuRepository.java
@@ -1,11 +1,16 @@
 package com.chothuesach.repository;
 
+import com.chothuesach.model.ChucVu;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.chothuesach.model.ChucVu;
+import java.util.List;
 
 @Repository
 public interface ChucVuRepository extends CrudRepository<ChucVu, Short> {
+
+    List<ChucVu> getChucVuByOrderByMaChucVu();
+
+    ChucVu getChucVuByTenChucVu(String tenChucVu);
 
 }
