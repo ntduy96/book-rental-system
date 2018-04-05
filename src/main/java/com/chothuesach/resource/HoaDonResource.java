73f63a8ead7f22c0cad0a1175355b0469ecdd59b
ntduy96 <ntduy96@gmail.com>
2018-04-05 19:23:17 +0700

    create resource class for HoaDon

---
+++ src/main/java/com/chothuesach/resource/HoaDonResource.java
@@ -0,0 +1,65 @@
+package com.chothuesach.resource;
+
+import com.chothuesach.dto.HoaDonDto;
+import com.chothuesach.jsonview.HoaDonView;
+import com.chothuesach.model.HoaDon;
+import com.chothuesach.service.HoaDonService;
+import com.chothuesach.service.NguoiDungService;
+import com.fasterxml.jackson.annotation.JsonView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.security.Principal;
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/api/hoadon")
+public class HoaDonResource {
+
+    @Autowired
+    private HoaDonService hoaDonService;
+
+    @Autowired
+    private NguoiDungService nguoiDungService;
+
+    @GetMapping
+    @JsonView(HoaDonView.Overview.class)
+    public Iterable<HoaDon> getAllHoaDon() {
+        return hoaDonService.getAllHoaDon();
+    }
+
+    @GetMapping("/cuatoi")
+    @JsonView(HoaDonView.Overview.class)
+    public Iterable<HoaDon> getMyHoaDon(Principal principal) {
+        return hoaDonService.getHoaDonOfKhachHang(nguoiDungService.getOneByUsername(principal.getName()));
+    }
+
+    @GetMapping("/{maHoaDon}")
+    @JsonView(HoaDonView.Detailed.class)
+    public HoaDon getHoaDonByMaHoaDon(@PathVariable String maHoaDon) {
+        return hoaDonService.getHoaDonByMaHoaDon(maHoaDon);
+    }
+
+    @PostMapping(consumes = "application/json")
+    @JsonView(HoaDonView.Detailed.class)
+    public ResponseEntity<Object> createNewHoaDon(@Valid @RequestBody HoaDonDto hoaDonDto, BindingResult result) {
+        if (result.hasErrors()) {
+            return new ResponseEntity<>(result.getAllErrors(), HttpStatus.BAD_REQUEST);
+        } else {
+            try {
+                HoaDon hoaDon = hoaDonService.taoHoaDon(hoaDonDto);
+                HashMap<String, Object> response = new HashMap<>();
+                response.put("created", "ok");
+                response.put("hoaDon", hoaDon);
+                return new ResponseEntity<>(response, HttpStatus.OK);
+            } catch (Exception e) {
+                return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+            }
+        }
+    }
+
+}
