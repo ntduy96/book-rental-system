e945755868a5594cda15f3afd658cb93d17249c4
ntduy96 <ntduy96@gmail.com>
2018-04-06 02:50:44 +0700

    create jsonview class for NhanVien

--- src/main/java/com/chothuesach/jsonview/NhanVienView.java
+++ src/main/java/com/chothuesach/jsonview/NhanVienView.java
@@ -1,6 +1,8 @@
 package com.chothuesach.jsonview;
 
 public class NhanVienView {
+
+	public interface Overview extends NguoiDungView.Overview {}
 	
 	public interface Detailed extends NguoiDungView.Detailed {}
 
