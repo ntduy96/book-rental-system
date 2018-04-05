ac277ed901f72d3e75da86fe0a72a599f046b86b
ntduy96 <ntduy96@gmail.com>
2018-04-05 19:15:16 +0700

    create repository for ChiTietHoaDon

---
+++ src/main/java/com/chothuesach/repository/ChiTietHoaDonRepository.java
@@ -0,0 +1,8 @@
+package com.chothuesach.repository;
+
+import com.chothuesach.model.ChiTietHoaDon;
+import com.chothuesach.model.ChiTietHoaDonId;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ChiTietHoaDonRepository extends CrudRepository<ChiTietHoaDon, ChiTietHoaDonId>{
+}
