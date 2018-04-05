ea3794379ecf47f872d1e5a447fbc4704d3e3b3f
ntduy96 <ntduy96@gmail.com>
2018-04-06 02:43:29 +0700

    add some crud methods in NhanVienRepository

--- src/main/java/com/chothuesach/repository/NhanVienRepository.java
+++ src/main/java/com/chothuesach/repository/NhanVienRepository.java
@@ -1,11 +1,27 @@
 package com.chothuesach.repository;
 
+import com.chothuesach.model.ChucVu;
+import com.chothuesach.model.NhanVien;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.chothuesach.model.NhanVien;
+import javax.transaction.Transactional;
+import java.util.List;
 
 @Repository
 public interface NhanVienRepository extends CrudRepository<NhanVien, String> {
+
+    List<NhanVien> getAllByOrderByHoTenNguoiDung();
+
+    List<NhanVien> getAllByChucVu(ChucVu chucVu);
+
+    NhanVien getByTenNguoiDung(String tenNguoiDung);
+
+    @Modifying
+    @Query("DELETE FROM NhanVien WHERE MA_NHAN_VIEN = ?1")
+    @Transactional
+    void deleteNhanVienByMaNguoiDung(String maNguoiDung);
 
 }
