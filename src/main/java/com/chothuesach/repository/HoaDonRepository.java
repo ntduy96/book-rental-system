13f93f64d633161de24b5f430aa63c0f7b26823a
ntduy96 <ntduy96@gmail.com>
2018-04-05 19:14:21 +0700

    add method for retrieve and delete hoadon

--- src/main/java/com/chothuesach/repository/HoaDonRepository.java
+++ src/main/java/com/chothuesach/repository/HoaDonRepository.java
@@ -1,9 +1,21 @@
 package com.chothuesach.repository;
 
-import org.springframework.data.repository.CrudRepository;
-
 import com.chothuesach.model.HoaDon;
+import com.chothuesach.model.NguoiDung;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public interface HoaDonRepository extends CrudRepository<HoaDon, String> {
+
+    List<HoaDon> getByKhachHangOrderByNgayLapHoaDonDesc(NguoiDung khachHang);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM HoaDon WHERE MA_HOA_DON = ?1")
+    void deleteHoaDonByMaHoaDon(String maHoaDon);
 
 }
