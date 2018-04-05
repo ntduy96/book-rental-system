b291e31c35b3262977d808353245d2a564215243
ntduy96 <ntduy96@gmail.com>
2018-04-05 19:19:25 +0700

    create not found exception class for HoaDon

---
+++ src/main/java/com/chothuesach/exception/HoaDonNotFoundException.java
@@ -0,0 +1,27 @@
+package com.chothuesach.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class HoaDonNotFoundException extends RuntimeException {
+
+    private String message = "Hoa don not found";
+
+    public HoaDonNotFoundException() {
+        
+    }
+
+    public HoaDonNotFoundException(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
