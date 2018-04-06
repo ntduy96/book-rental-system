e92195affe676141d905b35b50cfbd3bc59935e9
ntduy96 <ntduy96@gmail.com>
2018-04-06 22:38:51 +0700

    add angular ui router state

--- src/main/resources/static/js/management-app.js
+++ src/main/resources/static/js/management-app.js
@@ -1,1 +1,33 @@
-angular.module("managementApp", []);
+var app = angular.module("managementApp", ["ui.router"]);
+
+app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider.state({
+        name: "main",
+        url: "/",
+        templateUrl : "/html/main-management.html"
+    }).state({
+        name: "orders",
+        url: "/orders",
+        templateUrl : "/html/order-management.html"
+    }).state({
+        name: "library",
+        url: "/library",
+        templateUrl : "/html/library-management.html"
+    }).state({
+        name: "library.book",
+        url: "/book",
+        templateUrl : "/html/book-management.html"
+    }).state({
+        name: "library.author",
+        url: "/author",
+        templateUrl : "/html/author-management.html"
+    }).state({
+        name: "library.category",
+        url: "/category",
+        templateUrl : "/html/category-management.html"
+    });
+
+}]);
