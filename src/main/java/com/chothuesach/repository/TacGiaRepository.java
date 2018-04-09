5603efc010c91cc910d4705c58313978018c4741
ntduy96 <ntduy96@gmail.com>
2018-04-10 00:32:37 +0700

    add some methods in TacGiaRepository

--- src/main/java/com/chothuesach/repository/TacGiaRepository.java
+++ src/main/java/com/chothuesach/repository/TacGiaRepository.java
@@ -1,11 +1,18 @@
 package com.chothuesach.repository;
 
+import com.chothuesach.model.TacGia;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.chothuesach.model.TacGia;
+import java.util.List;
 
 @Repository
 public interface TacGiaRepository extends CrudRepository<TacGia, String> {
+
+    List<TacGia> findAll();
+
+    TacGia getBySlug(String slug);
+
+    TacGia getByTenTacGia(String tenTacGia);
 
 }
