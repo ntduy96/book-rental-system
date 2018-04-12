98ecea4914478b14ae16f3ca2bedef4473f00db5
ntduy96 <ntduy96@gmail.com>
2018-04-13 01:00:50 +0700

    check Optional and throw exception if not present

--- src/main/java/com/chothuesach/service/TheLoaiService.java
+++ src/main/java/com/chothuesach/service/TheLoaiService.java
@@ -1,11 +1,14 @@
 package com.chothuesach.service;
 
+import com.chothuesach.exception.ResourceNotFoundException;
 import com.chothuesach.model.TheLoai;
 import com.chothuesach.repository.TheLoaiRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TheLoaiService {
@@ -14,17 +17,35 @@ public class TheLoaiService {
     private TheLoaiRepository theLoaiRepository;
 
     public List<TheLoai> getDanhSachTheLoai() {
-        return theLoaiRepository.findAll();
+        return theLoaiRepository.findAll(new Sort(Sort.Direction.ASC, "tenTheLoai"));
     }
 
     public TheLoai getTheLoaiBySlug(String slug) {
-        return theLoaiRepository.getBySlug(slug);
+        Optional<TheLoai> result = theLoaiRepository.getBySlug(slug);
+        if (result.isPresent()) {
+            return result.get();
+        }
+        throw new ResourceNotFoundException("Can't found any TheLoai matching " + slug);
+    }
+
+    public TheLoai getTheLoaiByTenTheLoai(String tenTheLoai) {
+        Optional<TheLoai> result = theLoaiRepository.getByTenTheLoai(tenTheLoai);
+        if (result.isPresent()) {
+            return result.get();
+        }
+        throw new ResourceNotFoundException("Can't found any TheLoai matching " + tenTheLoai);
     }
 
     public void addNewTheLoai(String tenTheLoai) {
         TheLoai newTheLoai = new TheLoai();
         newTheLoai.setTenTheLoai(tenTheLoai);
         theLoaiRepository.save(newTheLoai);
+    }
+
+    public void changeTenTheLoai(String slug, String newTenTheLoai) {
+        TheLoai theLoai = getTheLoaiBySlug(slug);
+        theLoai.setTenTheLoai(newTenTheLoai);
+        theLoaiRepository.save(theLoai);
     }
 
 }
