b9bc6095f98c49068a4811aa8099d730c8078d49
ntduy96 <ntduy96@gmail.com>
2018-04-13 00:42:19 +0700

    modify body of error response

--- src/main/java/com/chothuesach/controller/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/chothuesach/controller/RestResponseEntityExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.chothuesach.controller;
 
 import com.chothuesach.error.ApiError;
+import com.chothuesach.exception.ResourceNotFoundException;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.InvalidDataAccessApiUsageException;
@@ -24,19 +25,19 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
 
     @ExceptionHandler({ DataIntegrityViolationException.class })
     public ResponseEntity<Object> handleBadRequest(final DataIntegrityViolationException ex, final WebRequest request) {
-        ApiError body = new ApiError(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), ex.getCause().getMessage());
+        ApiError body = new ApiError(HttpStatus.BAD_REQUEST, ex.getMessage(), ex.toString());
         return handleExceptionInternal(ex, body, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
 
     @Override
     protected ResponseEntity<Object> handleHttpMessageNotReadable(final HttpMessageNotReadableException ex, final HttpHeaders headers, final HttpStatus status, final WebRequest request) {
-        ApiError body = new ApiError(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), ex.getCause().getMessage());
+        ApiError body = new ApiError(HttpStatus.BAD_REQUEST, ex.getMessage(), ex.toString());
         return handleExceptionInternal(ex, body, headers, HttpStatus.BAD_REQUEST, request);
     }
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(final MethodArgumentNotValidException ex, final HttpHeaders headers, final HttpStatus status, final WebRequest request) {
-        ApiError body = new ApiError(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), ex.getCause().getMessage());
+        ApiError body = new ApiError(HttpStatus.BAD_REQUEST, ex.getMessage(), ex.toString());
         return handleExceptionInternal(ex, body, headers, HttpStatus.BAD_REQUEST, request);
     }
 
@@ -44,15 +45,22 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
     @ExceptionHandler({ AccessDeniedException.class })
     public ResponseEntity<Object> handleAccessDeniedException(final Exception ex, final WebRequest request) {
         System.out.println("request" + request.getUserPrincipal());
-        ApiError body = new ApiError(HttpStatus.FORBIDDEN, ex.getLocalizedMessage(), ex.getCause().getMessage());
+        ApiError body = new ApiError(HttpStatus.FORBIDDEN, ex.getMessage(), ex.toString());
         return new ResponseEntity<>(body, new HttpHeaders(), HttpStatus.FORBIDDEN);
+    }
+
+    // 404
+    @ExceptionHandler({ ResourceNotFoundException.class })
+    public ResponseEntity<Object> handleResourceNotFound(final ResourceNotFoundException ex, final WebRequest request) {
+        ApiError body = new ApiError(HttpStatus.NOT_FOUND, ex.getMessage(), ex.toString());
+        return handleExceptionInternal(ex, body, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
 
     // 409
 
     @ExceptionHandler({ InvalidDataAccessApiUsageException.class, DataAccessException.class })
-    protected ResponseEntity<Object> handleConflict(final RuntimeException ex, final WebRequest request) {
-        ApiError body = new ApiError(HttpStatus.CONFLICT, ex.getLocalizedMessage(), ex.getCause().getMessage());
+    public ResponseEntity<Object> handleConflict(final RuntimeException ex, final WebRequest request) {
+        ApiError body = new ApiError(HttpStatus.CONFLICT, ex.getMessage(), ex.toString());
         return handleExceptionInternal(ex, body, new HttpHeaders(), HttpStatus.CONFLICT, request);
     }
 
@@ -63,7 +71,7 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
     @ExceptionHandler({ NullPointerException.class, IllegalArgumentException.class, IllegalStateException.class })
     /*500*/public ResponseEntity<Object> handleInternal(final RuntimeException ex, final WebRequest request) {
         logger.error("500 Status Code", ex);
-        ApiError body = new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, ex.getLocalizedMessage(), ex.getCause().getMessage());
+        ApiError body = new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), ex.toString());
         return handleExceptionInternal(ex, body, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
     }
 
