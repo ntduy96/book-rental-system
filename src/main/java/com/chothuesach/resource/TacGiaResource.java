e622dcca836e961675bcbf03c1fd7883e62caa47
ntduy96 <ntduy96@gmail.com>
2018-04-13 00:58:27 +0700

    initialize TacGiaResource

---
+++ src/main/java/com/chothuesach/resource/TacGiaResource.java
@@ -0,0 +1,46 @@
+package com.chothuesach.resource;
+
+import com.chothuesach.jsonview.TacGiaView;
+import com.chothuesach.model.TacGia;
+import com.chothuesach.service.TacGiaService;
+import com.fasterxml.jackson.annotation.JsonView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tacgia")
+public class TacGiaResource {
+
+    @Autowired
+    private TacGiaService tacGiaService;
+
+    @GetMapping
+    @JsonView(TacGiaView.Overview.class)
+    public List<TacGia> getAll() {
+        return tacGiaService.getDanhSachTacGia();
+    }
+
+    @GetMapping("/{slug}")
+    @JsonView(TacGiaView.Detailed.class)
+    public TacGia getOneBySlug(@PathVariable String slug) {
+        return tacGiaService.getOneBySlug(slug);
+    }
+
+    @PostMapping
+    public void createNewTacGia(@RequestParam String tenTacGia) {
+        tacGiaService.addNewTacGia(tenTacGia);
+    }
+
+    @PutMapping("/{slug}")
+    public void updateTenTacGia(@PathVariable String slug, @RequestParam String newTenTacGia) {
+        tacGiaService.changeTenTacGia(slug, newTenTacGia);
+    }
+
+    @DeleteMapping("/{slug}")
+    public void deleteTacGia(@PathVariable String slug) {
+        tacGiaService.deleteTacGia(slug);
+    }
+
+}
