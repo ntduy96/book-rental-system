f096340b425f5b3936c384c05f2238c88c5fc3ec
ntduy96 <ntduy96@gmail.com>
2018-04-14 01:22:35 +0700

    change return type from NguoiDung to Optional<NguoiDung>

--- src/main/java/com/chothuesach/repository/NguoiDungRepository.java
+++ src/main/java/com/chothuesach/repository/NguoiDungRepository.java
@@ -1,10 +1,11 @@
 package com.chothuesach.repository;
 
+import com.chothuesach.model.NguoiDung;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.chothuesach.model.NguoiDung;
+import java.util.Optional;
 
 @Repository
 public interface NguoiDungRepository extends CrudRepository<NguoiDung, String> {
@@ -12,10 +13,10 @@ public interface NguoiDungRepository extends CrudRepository<NguoiDung, String> {
 	@Query(value = "SELECT PASSWORD FROM NGUOI_DUNG WHERE MA_NGUOI_DUNG = ?1", nativeQuery = true)
 	public String getPasswordByUsername(String tenNguoiDung);
 	
-	NguoiDung findOneByTenNguoiDung(String tenNguoiDung);
-	
-	NguoiDung findOneByEmail(String email);
+	Optional<NguoiDung> findOneByTenNguoiDung(String tenNguoiDung);
 
-	NguoiDung findOneBySoCmnd(String soCmnd);
+	Optional<NguoiDung> findOneByEmail(String email);
+
+	Optional<NguoiDung> findOneBySoCmnd(String soCmnd);
 
 }
