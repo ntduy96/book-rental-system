5dafd07346ccee0702f9a2c1cf6faf2281006978
ntduy96 <ntduy96@gmail.com>
2018-04-13 14:59:00 +0700

    get tenNguoiDung in session rather than manually from dto

--- src/main/java/com/chothuesach/dto/HoaDonDto.java
+++ src/main/java/com/chothuesach/dto/HoaDonDto.java
@@ -7,19 +7,8 @@ import java.util.List;
 public class HoaDonDto {
 
     @NotEmpty
-    private String maKhachHang;
-
-    @NotEmpty
     @Valid
     private List<ChiTietHoaDonDto> chiTietHoaDonList;
-
-    public String getMaKhachHang() {
-        return maKhachHang;
-    }
-
-    public void setMaKhachHang(String maKhachHang) {
-        this.maKhachHang = maKhachHang;
-    }
 
     public List<ChiTietHoaDonDto> getChiTietHoaDonList() {
         return chiTietHoaDonList;
