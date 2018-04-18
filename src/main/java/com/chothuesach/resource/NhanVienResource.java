916de56bdbe92cbcb1d9dc4c9428dbfad28af7d0
ntduy96 <ntduy96@gmail.com>
2018-04-19 00:18:12 +0700

    add updateNhanVien()

--- src/main/java/com/chothuesach/resource/NhanVienResource.java
+++ src/main/java/com/chothuesach/resource/NhanVienResource.java
@@ -1,6 +1,7 @@
 package com.chothuesach.resource;
 
 import com.chothuesach.dto.NhanVienDto;
+import com.chothuesach.dto.NhanVienUpdateDto;
 import com.chothuesach.jsonview.NhanVienView;
 import com.chothuesach.model.NhanVien;
 import com.chothuesach.service.NhanVienService;
@@ -58,9 +59,25 @@ public class NhanVienResource {
     }
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @DeleteMapping("/{maNhanVien}")
-    public ResponseEntity deleteNhanVien(@PathVariable String maNhanVien) {
-        nhanVienService.deleteNhanVien(maNhanVien);
+    @PutMapping(value = "/{tenNguoiDung}", consumes = "application/json")
+    @JsonView(NhanVienView.Detailed.class)
+    public ResponseEntity updateNhanVien(@PathVariable String tenNguoiDung, @RequestBody @Valid NhanVienUpdateDto nhanVienUpdateDto, BindingResult result) {
+        if (result.hasErrors()) {
+            return new ResponseEntity<>(result.getAllErrors(), HttpStatus.BAD_REQUEST);
+        } else {
+            try {
+                NhanVien nhanVien = nhanVienService.updateNhanVien(tenNguoiDung, nhanVienUpdateDto);
+                return new ResponseEntity<>(nhanVien, HttpStatus.OK);
+            } catch (Exception e) {
+                return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+            }
+        }
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @DeleteMapping("/{tenNhanVien}")
+    public ResponseEntity deleteNhanVien(@PathVariable String tenNhanVien) {
+        nhanVienService.deleteNhanVien(tenNhanVien);
         HashMap<String, Object> response = new HashMap<>();
         response.put("deleted", "ok");
         return new ResponseEntity<>(response, HttpStatus.OK);
