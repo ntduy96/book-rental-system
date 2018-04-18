dde5993f24cf5c31532b0d356690e15aa6ce877e
ntduy96 <ntduy96@gmail.com>
2018-04-19 01:44:54 +0700

    add jsonview for role

--- src/main/java/com/chothuesach/model/Role.java
+++ src/main/java/com/chothuesach/model/Role.java
@@ -1,16 +1,12 @@
 package com.chothuesach.model;
 
+import com.chothuesach.jsonview.NguoiDungView;
+import com.fasterxml.jackson.annotation.JsonView;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.Collection;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.validation.constraints.NotNull;
 
 @Entity
 public class Role implements Serializable {
@@ -22,6 +18,7 @@ public class Role implements Serializable {
 	private Long roleId;
 
 	@NotNull
+	@JsonView(NguoiDungView.Detailed.class)
 	private String roleName;
 
 	@NotNull
