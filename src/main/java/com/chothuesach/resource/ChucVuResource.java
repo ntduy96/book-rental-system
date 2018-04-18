a8050f6ec5fd7c304bc07bcb5fc425c81435d796
ntduy96 <ntduy96@gmail.com>
2018-04-18 20:19:18 +0700

    initialize ChucVuResource

---
+++ src/main/java/com/chothuesach/resource/ChucVuResource.java
@@ -0,0 +1,34 @@
+package com.chothuesach.resource;
+
+import com.chothuesach.jsonview.ChucVuView;
+import com.chothuesach.model.ChucVu;
+import com.chothuesach.service.ChucVuService;
+import com.fasterxml.jackson.annotation.JsonView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/chucvu")
+public class ChucVuResource {
+
+    @Autowired
+    private ChucVuService chucVuService;
+
+    @GetMapping
+    @JsonView(ChucVuView.Overview.class)
+    public List<ChucVu> getAllChucVu() {
+        return chucVuService.getAllChucVu();
+    }
+
+    @GetMapping("/{tenChucVu}")
+    @JsonView(ChucVuView.Detailed.class)
+    public ChucVu getChucVuByTenChucVu(@PathVariable String tenChucVu) {
+        return chucVuService.getByTenChucVu(tenChucVu);
+    }
+
+}
